a543491bb274f8e43db0f9674996ae75b88efbff
SYTSMA4 <sytsma4@tritons.iowacentral.edu>
2022-09-12 09:57:08 -0500

    Initial Commit

---
+++ Lab3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab3
+//
+//  Created by Sam Sytsma on 9/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
